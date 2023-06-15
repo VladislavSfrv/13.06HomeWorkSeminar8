b45c3742666f35b20ffdfd9d738751f387336d22
unknown <vladislavsafarov04@mail.ru>
2023-06-15 13:38:18 +0300

    Added Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j != matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] SortToMinMatrix(int[,] matrix)
+{
+    int temp = matrix[0, 0];
+    int max = matrix[0, 0];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1;k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }       
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+PrintMatrix(SortToMinMatrix(matrix));
